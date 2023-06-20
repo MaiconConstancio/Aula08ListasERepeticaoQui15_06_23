3b29c2357a1101c416cd4a9b1dfc5bc7db3c9764
MaiconConstancio <135743035+MaiconConstancio@users.noreply.github.com>
2023-06-20 14:36:36 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,145 +4,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atividade07MaquinaDeCafe01
+namespace Aula08ListasERepeticaoQui
 {
     class Program
     {
-        static double novoVapCash = 0;
 
         static void Main(string[] args)
         {
-            string nomeDaEmpresa = "MK VACAP";
-            double vapCash = 0;
-            //double novoVapCash = 0; ESSE N É MAIS USADO, PORQUE TEM UM FORA DO VOID, QUE PERMITE RECONHECER O VALOR DEPOIS QUE O SISTEMA REINICIOU
-            string confirmaçãoDoDinheiro = "";
+            List<string> nomes = new List<string>();
+            nomes.Add("Kennedy");
+            nomes.Add("nath");
+            nomes.Add("du");
+            nomes.Add("Mirela");
+            nomes.Add("Mirela");
+            nomes.Add("Mirela");
+            nomes.Add("Mirela");
+            nomes.Add(Console.ReadLine());
 
-            //---------- ALTERAR VALOR E OPÇOES DO CARDAPIO AQUI ----------------
-            string opcao01 = "Café expresso";
-            double valorOpcao01 = 3.00;
-
-            string opcao02 = "Capuccino";
-            double valorOpcao02 = 4.50;
-
-            string opcao03 = "Mocaccino";
-            double valorOpcao03 = 6.00;
-
-            string opcao04 = "Água quente";
-            double valorOpcao04 = 1.00;
-            //------------- OPÇOES PARA A ESCOLHA DOS PRODUTOS ACIMA ------------
-            string OpcaoEscolhida = "";
-            string selecionarOpcao01 = "01";
-            string selecionarOpcao02 = "02";
-            string selecionarOpcao03 = "03";
-            string selecionarOpcao04 = "04";
-
-
-            // --------------------- SESSÃO DE ERRO ---------------------------------
-            string ERROR = ("Desculpe, não existe essa opção, vamos reiniciar o sistema e tentar novamente!\n" +
-                "        -------------------------------\n\n");
-
-            string ERRORnao = ("Certo, vamos reiniciar o sistema e tentar novamente!\n" +
-                "        -------------------------------\n\n");
-
-            string ERRORsaldo = ("Saldo insuficiente, por favor, recarregue e tente novamente!\n" +
-                "        -------------------------------\n\n");
-
-            //------------------------------------ INICIO DO CODIGO -----------------------------------------------
-
-            Console.WriteLine($"Ultimo usuario tem R${novoVapCash}, de saldo para a proxima compra.\n");
-
-            Console.WriteLine($"Olá seja Bem-vindo ao sistema {nomeDaEmpresa}");
-            Console.WriteLine("Nosso sistema usa o mais novo e avançado VACAP CASH, que consiste em " +
-                "colocar créditos antecipadamente, e só no final do mês, debitamos do seu cartão");
-
-            //---------------------  VALOR DE CREDITO QUE O CLIENTE COLOCA ---------------------
-
-            Console.WriteLine("Então vamos lá? Qual o valor você gostaria de aplicar?");
-            vapCash = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Valor total: " + vapCash + " reais.");
-            Console.WriteLine("O valor aplicado está correto?");
-            Console.WriteLine("Dirite 'Sim' ou 'Não'");
-            confirmaçãoDoDinheiro = Console.ReadLine();
-
-            //---------------------  CONFIRMAÇÃO SOBRE O VALOR APLICADO ---------------------
-
-            if (confirmaçãoDoDinheiro == "SIM" || confirmaçãoDoDinheiro == "sim" || confirmaçãoDoDinheiro == "Sim"
-                || confirmaçãoDoDinheiro == "SIm" || confirmaçãoDoDinheiro == "sIM" || confirmaçãoDoDinheiro == "s")
+            int quantidade = nomes.Count;
+            int contador = 0;
+            while (contador < quantidade)
             {
-                Console.WriteLine($"Ótimo! valor de {vapCash} reais, confirmado com sucesso!");
-                Console.WriteLine("        -------------------------------\n\n");
-            }
-            else if (confirmaçãoDoDinheiro == "NÃO" || confirmaçãoDoDinheiro == "NAO" || confirmaçãoDoDinheiro == "Não"
-                || confirmaçãoDoDinheiro == "Nao" || confirmaçãoDoDinheiro == "nÃO" || confirmaçãoDoDinheiro == "nAO"
-                || confirmaçãoDoDinheiro == "não" || confirmaçãoDoDinheiro == "nao" || confirmaçãoDoDinheiro == "n")
-            {
-                Console.WriteLine(ERRORnao);
-                Main(null);
-                return;
+                Console.WriteLine($"Nome: {nomes[contador]}");
+                contador += 1;
             }
 
-            else
+            Console.ReadKey();
+
+        }
+
+        void repetição()
+        {
+            //Lado de repetição (LOOP)
+            int contador = 0;
+
+            while (contador < 5)
             {
-                Console.WriteLine(ERROR);
-                Main(null);
-                return;
+                Console.WriteLine("Olá!");
+
+                contador += 1;
             }
 
-            // ---------------------------------- OPÇÕES DE CAFÉ ---------------------------------------------
+            Console.ReadKey();
 
-            Console.WriteLine("Escolha uma opção abaixo, pelo numero equivalentes:");
-            Console.WriteLine("Exemplo: '07'\n");
-            Console.WriteLine(selecionarOpcao01 + " - " + opcao01 + " - R$ " + valorOpcao01);
-            Console.WriteLine(selecionarOpcao02 + " - " + opcao02 + " - R$ " + valorOpcao02);
-            Console.WriteLine(selecionarOpcao03 + " - " + opcao03 + " - R$ " + valorOpcao03);
-            Console.WriteLine(selecionarOpcao04 + " - " + opcao04 + " - R$ " + valorOpcao04);
-            OpcaoEscolhida = Console.ReadLine();
 
-            if (OpcaoEscolhida == selecionarOpcao01 || OpcaoEscolhida == "1")
-            {
-                Console.WriteLine(opcao01 + " selecionado!");
-                novoVapCash = vapCash - valorOpcao01;
-            }
-            else if (OpcaoEscolhida == selecionarOpcao02 || OpcaoEscolhida == "2")
-            {
-                Console.WriteLine(opcao02 + " selecionado!");
-                novoVapCash = vapCash - valorOpcao02;
-            }
-            else if (OpcaoEscolhida == selecionarOpcao03 || OpcaoEscolhida == "3")
-            {
-                Console.WriteLine(opcao03 + " selecionado!");
-                novoVapCash = vapCash - valorOpcao03;
-            }
-            else if (OpcaoEscolhida == selecionarOpcao04 || OpcaoEscolhida == "4")
-            {
-                Console.WriteLine(opcao04 + " selecionado!");
-                novoVapCash = vapCash - valorOpcao04;
-            }
-            else
-            {
-                Console.WriteLine(ERROR);
-                Main(null);
-                return;
+            //string nome;
 
-            }
-            // -------------------------- SOBRA DO DINHEIRO DEPOIS DA ESCOLHA DE CAFÉ ------------------------
-            if (novoVapCash >= 0)
-            {
-                Console.WriteLine($"Saldo atual {novoVapCash} reais, seu pedido sera realizado!");
-            }
-            else
-            {
-                Console.WriteLine(ERRORsaldo);
-                Main(null);
-                return;
-            }
-            Console.WriteLine("Sistema encerrado, vamos para o proximo pedido\n" +
-                "        -------------------------------\n\n");
 
-            Main(null);
-            return;
+            //Console.WriteLine("Digite TESTE para aparecer a mensagem\n");
+            //nome = Console.ReadLine();
 
+            //while (nome == "TESTE" || nome == "teste")
+            //{
+            //    Console.WriteLine("\nHello Word!\n");
+            //    Main(null);
+            //    return;
+            //}
+
+
+
+            //int pontos = 0;
+
+            //while (pontos > 21)
+            //{
+            //    Console.WriteLine("Você quer mais cartas?");
+            //}
+
+        }
+
+        void List()
+        {
+            //Listas -> Mesmo que o Array, porém com tamanho automático
+            // Add (Item)      => adiciona um item novo na lista
+            // Remove (item)   => remove um item pelo valor dele na lista
+            // RemoveAt (item) => Remove um item pelo numero da posição dele
+            // Count           => Mostra o número de itens que existem na lista
+
+            List<string> nomes = new List<string>(); // fora do void jogou static antes da list
+
+
+            nomes.Add("Jonatas"); //0
+            nomes.Add("Natanael"); //1
+            nomes.Add("Bruno"); //2
+
+            Console.WriteLine($"O nome da primeira é {nomes[0]}");
+            Console.WriteLine($"O nome do segundo é {nomes[1]}");
+            Console.WriteLine($"O nome do terceiro é {nomes[2]}");
+
+            Console.WriteLine("Qual o seu nome?");
+            nomes.Add(Console.ReadLine()); // ADICIONA UM NOVO NOME
+
+            Console.WriteLine($"O nome do quarto nome é {nomes[3]}"); // PUXA O NOME PELA POSIÇÃO
+
+            nomes.Remove("Natanael");
+            //nomes.RemoveAt(3);
+
+
+
+
+            //Esse codigo serve para contar quantos nomes tem dentro de uma lista de nomes
+
+            int quantidade = nomes.Count;
+            Console.WriteLine($"Existem {quantidade} usuários cadastrados!");
+
+            //----------------OU DA PRA FAZER ASSIM TBM------------------------
+
+            Console.WriteLine($"Existem {nomes.Count} usuários cadastrados!");
+
+            Console.ReadKey();
+        }
+
+        void Arrays()
+        { 
+            //Arrey ou vetor -> Conjunto de dados, da pra adicionar depois com o readline
+            string[] nomes = new string[3];
+            //OU DA PRA FAZER ASSIM, POREM DESSE JEITO N DA PRA ADICIONAR DEPOIS COM O READLINE
+            int[] idades = { 19, 21, 18 };
+
+
+            nomes[0] = "kennedy";
+            nomes[1] = "nath";
+            nomes[2] = "du";
+
+            Console.WriteLine($"O nome da primeira posição é {nomes[1]}");
+            Console.WriteLine($"a idade é {idades[0]}");
 
             Console.ReadKey();
         }
